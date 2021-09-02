1b58967750051ad8056fa803bf00f46b61e8ba7a
Ved <vedpat3@gmail.com>
2021-09-02 22:06:23 +0530

    Integrated Event Details page

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+(function($) {
+
+    "use strict";
+
+    var fullHeight = function() {
+
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function() {
+            $('.js-fullheight').css('height', $(window).height());
+        });
+
+    };
+    fullHeight();
+
+    var carousel = function() {
+        $('.featured-carousel').owlCarousel({
+            loop: true,
+            autoplay: true,
+            margin: 30,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            nav: true,
+            dots: true,
+            autoplayHoverPause: false,
+            items: 1,
+            navText: ["<span class='ion-ios-arrow-back'></span>", "<span class='ion-ios-arrow-forward'></span>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                },
+                1100: {
+                    items: 3
+                },
+                1200: {
+                    items: 3
+                },
+                1300: {
+                    items: 3
+                },
+                1400: {
+                    items: 3
+                }
+            }
+        });
+
+    };
+    carousel();
+
+})(jQuery);
